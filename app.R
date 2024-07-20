24a0ca4ba475c3a55b006af4e34ac90021b8ffea
winstonsieck <sieck@globalcognition.org>
2024-07-20 10:07:24 -0400

    define outputs

--- app.R
+++ app.R
@@ -10,6 +10,15 @@ ui <- fluidPage(
 )
 
 server <- function(input,output,session) {
+  output$summary <- renderPrint({
+    dataset <- get(input$dataset, 'package:datasets')
+    summary(dataset)
+  })
+  
+  output$table <- renderTable({
+    dataset <- get(input$dataset, 'package:datasets')
+    dataset
+  })
 }
 
 shinyApp(ui,server)
